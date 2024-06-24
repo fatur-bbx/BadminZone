61556ab9b19f4551729d5d7cfcad681971a8d7eb
Fatur Rizqy Alfarisz <fatur21ti@mahasiswa.pcr.ac.id>
2024-06-24 10:02:54 +0700

    Perbaikan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // AkunSeeder::class,
+            AkunSeeder::class,
             PersediaanSeeder::class,
             PengeluaranSeeder::class,
             PendapatanSeeder::class,
